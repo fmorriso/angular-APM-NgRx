91d7b3c7f31c77d6761821b961af98ad269903d9
Fred Morrison <fpmorrison@verizon.net>
2020-04-12 11:56:06 -0400

    keep track of showProductCode in NgRx state

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
+import { Store, select } from '@ngrx/store';
 
 @Component({
 	selector: 'pm-product-list',
@@ -22,7 +23,10 @@ export class ProductListComponent implements OnInit, OnDestroy {
 	selectedProduct: Product | null;
 	sub: Subscription;
 
-	constructor(private productService: ProductService) {}
+	constructor(
+		private store: Store<any>,
+		private productService: ProductService
+	) {}
 
 	ngOnInit(): void {
 		this.sub = this.productService.selectedProductChanges$.subscribe(
@@ -33,6 +37,17 @@ export class ProductListComponent implements OnInit, OnDestroy {
 			next: (products: Product[]) => (this.products = products),
 			error: (err: any) => (this.errorMessage = err.error),
 		});
+
+		// subscribe to any changes to the 'products' portion/slice of the NgRx state
+		// TODO: Unsubscribe
+		this.store
+			.pipe(select('products'))
+			//
+			.subscribe((products) => {
+				if (products) {
+					this.displayCode = products.showProductCode;
+				}
+			});
 	}
 
 	ngOnDestroy(): void {
@@ -40,7 +55,11 @@ export class ProductListComponent implements OnInit, OnDestroy {
 	}
 
 	checkChanged(value: boolean): void {
-		this.displayCode = value;
+		// this.displayCode = value;
+		this.store.dispatch({
+			type: 'TOGGLE_PRODUCT_CODE',
+			payload: value,
+		});
 	}
 
 	newProduct(): void {
