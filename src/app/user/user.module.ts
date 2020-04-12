49bf41df6e2619adf0019eff156c6bcd15e231f5
Fred Morrison <fpmorrison@verizon.net>
2020-04-12 14:40:00 -0400

    add mask user name to NgRx store

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,10 +5,19 @@ import { SharedModule } from '../shared/shared.module';
 
 import { LoginComponent } from './login.component';
 
+/* NgRx */
+import { StoreModule } from '@ngrx/store';
+//
+import { reducer } from './state/user.reducer';
+
 const userRoutes: Routes = [{ path: 'login', component: LoginComponent }];
 
 @NgModule({
-	imports: [SharedModule, RouterModule.forChild(userRoutes)],
+	imports: [
+		SharedModule,
+		RouterModule.forChild(userRoutes),
+		StoreModule.forFeature('users', reducer),
+	],
 	declarations: [LoginComponent],
 })
 export class UserModule {}
