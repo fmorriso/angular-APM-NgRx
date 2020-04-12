8776107f9dcb6e99ffe6f80599f3b37bdabcc30b
Fred Morrison <fpmorrison@verizon.net>
2020-04-12 11:35:49 -0400

    define store and product reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,9 @@ import { HttpClientModule } from '@angular/common/http';
 // Imports for loading & configuring the in-memory web api
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { ProductData } from './products/product-data';
-
+//
 import { AppRoutingModule } from './app-routing.module';
-
+//
 import { AppComponent } from './app.component';
 import { ShellComponent } from './home/shell.component';
 import { MenuComponent } from './home/menu.component';
@@ -17,6 +17,9 @@ import { PageNotFoundComponent } from './home/page-not-found.component';
 /* Feature Modules */
 import { UserModule } from './user/user.module';
 
+/* NgRx */
+import {StoreModule} from '@ngrx/store';
+
 @NgModule({
 	imports: [
 		BrowserModule,
@@ -24,6 +27,7 @@ import { UserModule } from './user/user.module';
 		HttpClientInMemoryWebApiModule.forRoot(ProductData),
 		UserModule,
 		AppRoutingModule,
+		StoreModule.forRoot({})
 	],
 	declarations: [
 		AppComponent,
