56667fa4171202ea79820484d1fcf35276d115ef
Fred Morrison <fpmorrison@verizon.net>
2020-04-12 15:06:50 -0400

    add developer tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,9 @@ import { PageNotFoundComponent } from './home/page-not-found.component';
 import { UserModule } from './user/user.module';
 
 /* NgRx */
-import {StoreModule} from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
 	imports: [
@@ -27,7 +29,12 @@ import {StoreModule} from '@ngrx/store';
 		HttpClientInMemoryWebApiModule.forRoot(ProductData),
 		UserModule,
 		AppRoutingModule,
-		StoreModule.forRoot({})
+		StoreModule.forRoot({}),
+		StoreDevtoolsModule.instrument({
+			name: 'APM Demo App DevTools',
+			maxAge: 25,
+			logOnly: environment.production,
+		}),
 	],
 	declarations: [
 		AppComponent,
